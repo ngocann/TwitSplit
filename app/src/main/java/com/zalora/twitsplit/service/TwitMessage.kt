a8cc930c32ef3589b1bbbf5ff294250c245a4b13
hidden1611 <ann@elcom.com.vn>
2018-06-15 12:01:44 +0700

    init project

---
+++ app/src/main/java/com/zalora/twitsplit/service/TwitMessage.kt
@@ -0,0 +1,40 @@
+package com.zalora.twitsplit.service
+
+import java.util.ArrayList
+
+class TwitMessage(var index : Int, var total : Int) {
+
+    val MAX_MESSAGE = 50
+    var wordList : ArrayList<String> = ArrayList()
+
+    constructor(index : Int, total : Int, word : String) : this(index, total) {
+        wordList.add(word)
+    }
+    /**
+     * return True: can add new word
+     *        False : can not add, break new message
+     */
+    fun checkLargerAndAdd(word : String) : Boolean{
+        return if (!isLargeLimit(word)) {
+            false
+        } else {
+            wordList.add(word)
+            true
+        }
+    }
+
+    /**
+     * return True: can add new word
+     *        False : can not add, break new message
+     */
+    fun isLargeLimit(word : String) : Boolean {
+        val length = wordList.joinToString(" ","$index/$total ").length
+        val newLength = length + word.length
+        return newLength <= MAX_MESSAGE
+    }
+
+    override fun toString(): String {
+        return wordList.joinToString(" ","$index/$total ")
+    }
+
+}
---
+++ app/src/test/java/com/zalora/twitsplit/service/TwitSplitServiceTest.kt
@@ -0,0 +1,22 @@
+package com.zalora.twitsplit.service
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TwitSplitServiceTest {
+
+    @Test
+    fun splitMessage() {
+
+        var twitSplit = TwitSplitService()
+        var message = "I can't believe Tweeter now supports chunking my messages, so I don't have to do it myself."
+        var listTwitMessage = twitSplit.splitMessage(message)
+        assertEquals("1/2 I can't believe Tweeter now supports chunking", listTwitMessage[0])
+        assertEquals("2/2 my messages, so I don't have to do it myself.", listTwitMessage[1])
+
+        message = "I can't believe Tweeter now supports chunking"
+        listTwitMessage = twitSplit.splitMessage(message)
+        assertEquals("I can't believe Tweeter now supports chunking", listTwitMessage[0])
+    }
+}
